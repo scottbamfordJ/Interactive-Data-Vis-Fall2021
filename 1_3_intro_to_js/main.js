c1225d35b34528ec2d2cde703e4a5cb42df51e06
scottbamfordJ <75759730+scottbamfordJ@users.noreply.github.com>
2021-09-02 17:17:16 -0400

    Assignment 1_3

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+var count = 0
+var button_clicks = document.getElementById("counting_clicks")
+var reset_clicks = document.getElementById('reset')
+button_clicks.onclick = function() {
+    count+= 1;
+    this.innerHTML = count;
+}
+
+reset_clicks.onclick = function() {
+    count = 0
+    button_clicks.innerHTML = count
+}
