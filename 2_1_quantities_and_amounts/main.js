d2a144932b81f8d1fa2b12da75e19390431fe2ea
scottbamfordJ <75759730+scottbamfordJ@users.noreply.github.com>
2021-10-03 21:20:39 -0400

    Assignment 2_1 Updated

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -1,13 +1,18 @@
 /* CONSTANTS AND GLOBALS */
-const width  = 900
-const height  = 500
-margin = ({
+
+var margin = {
   top: 10,
   right: 10,
   bottom: 20,
   left: 35
-})
-
+ }, Width = 900 - margin.left - margin.right, Height = 500 -margin.top - margin.bottom
+var  margins = {
+  top: 20, 
+  right: 200,
+  bottom: 40,
+  left: 90
+}, width = 900 - margins.left - margins.right,
+height = 500 - margins.top - margins.bottom;
 /* LOAD DATA */
 d3.csv('CountryData.csv', d3.autoType)
   .then(data => {
@@ -15,30 +20,26 @@ d3.csv('CountryData.csv', d3.autoType)
     //console.log(data , d=> d.Average_Deaths)
     const svg = d3.select("#container")
       .append("svg")
-      .attr("width", width)
-      .attr("height", height)
+      .attr("width", Width)
+      .attr("height", Height)
       .style("background-color", "pink")
     const attributes = data.map(d => d.Name_Of_Region)
 /* SCALES */
 /** This is where you should define your scales from data to pixel space */
     const xScale = d3.scaleBand()
       .domain(attributes)
-      .range([ margin.left, width - margin.right - margin.left ])
+      .range([ margin.left, Width - margin.right - margin.left ])
       .paddingInner(.3);
 
     const yScale = d3.scaleLinear()
       .domain(d3.extent(data, d => d.Average_Deaths))
-      .range([ height - margin.bottom, margin.top ])
+      .range([ Height - margin.bottom, margin.top ])
       .nice()
     xAxis = d3.axisBottom(xScale)
       .tickSizeOuter(0)
     yAxis = d3.axisLeft(yScale)
       .tickSizeOuter(0)
-//["Central America","Southeast Asia", "North America", "South America", "Western Europe", "Sub-Saharan Africa", "Middle East & North Africa", "East Asia", "Australasia & Oceania", "South Asia", "Central Asia"]
-    // console.log('xScale("Western Europe") :>>', xScale("Western Europe"));
-    // console.log('xScale("Southeast Asia") :>>', xScale("Southeast Asia"));
-    // console.log('xScale("Central America & Caribbean") :>>', xScale("Central America & Caribbean"));
-    
+
 //     /* HTML ELEMENTS */
 //     /** Select your container and append the visual elements to it */
     svg.selectAll(".bar")
@@ -49,84 +50,58 @@ d3.csv('CountryData.csv', d3.autoType)
       .attr("y", d => yScale(d.Average_Deaths))
       .attr("width", xScale.bandwidth())
       .attr("height", d => yScale(0) - yScale(d.Average_Deaths))
-    // svg.append("g")
-    //   .attr("class", "axis axis --x")
-    //   .attr("transform", "translat(0, " + 600 + ")")
-    //   .call(d3.axisBottom(xScale))
-    //   .attr("dx", "-.25em")
-    //   .attr("transform", "rotate(-90")
     svg.append('g')
       .attr('class', 'x-axis')
-      .attr('transform', `translate(0,${ height - margin.bottom })`)
+      .attr('transform', `translate(0,${ Height - margin.bottom })`)
       .attr("dx", "1em")
       .call( xAxis )
     svg.append('g')
       .attr('class', 'y-axis')
       .attr('transform', `translate(${ margin.left },0)`)
       .call( yAxis )
-    // svg.selectAll(".text")
-    //   .data(data)
-    //   .enter()
-    //   .append("text")
-    //   .attr("clss", "label")
-    //   .attr("x", (function(d) {return xScale(d.Name_Of_Region); } ))
-    //   .attr("y", (function (d) { return yScale(d.Average_Deaths) - 20 ; } ))
-    //   .attr("dx", "-.25em")
-    //   .attr("dy", ".25em")
-    //   .attr("transform", "rotate(-90)" )
-    //   .text(function(d) {return [d.Name_Of_Region]})
-  
-
-    // svg.append("text")
-    //   .attr("class", "x label")
-    //   .attr("text-ancor", "end")
-    //   .attr("x", (function(d) {return xScale(d.Name_Of_Region); })
-    //   .attr("y", (function (d) { return yScale(d.Average_Deaths) - 20; })
-    //   .text("Countries")
-
-
-
-
 })
 
-    
-
-//     const table = d3.select("#container")
-//       .append("table")
-    
-    
-//     const rows = table
-//       .selectAll(".row")
-//       .data(data)
-//       .join("tr")
-//       .attr("class", "row")
-
-//     const cells = rows
-//       .selectAll(".cell")
-//       .data(d => Object.values(d))
-//       .join("td")
-//       .text(d => d)
-//   })
-
-// const svg = d3.select("svg")
-// const xScale = d3.scaleLinear()
-//   .domain([0,1])
-//   .range([0, width])
-// const yScale = d3.scaleLinear()
-//   .domain([0,1])
-//   .range([height, 0])
-// const xAxis = (g) => g
-//   .attr('transform', 'translate(0,${height})')
-//   .call(d3.axisBottom(xScale))
-// const yAxis = (g) => g 
-//   .call(d3.axisLeft(yScale))
-// svg.append("g")
-//   .call(xAxis);
-// svg.append("g")
-//   .call(yAxis)
 
 
-    
+var svg = d3.select("#container1")
+  .append("svg")
+    .attr("width", width + margins.left + margins.right)
+    .attr("height", height + margins.top + margins.bottom)
+  .append("g")
+    .attr("transform", "translatee(" + margins.left + "," + margins.top + ")");
+d3.csv("CountryData.csv", )
+  .then(data =>  {
+
+  var x = d3.scaleLinear()
+    //.domain(d3.extent(data, d => d.Average_Deaths))
+    .domain(d3.extent(data, d => d.Average_Deaths))
+    .range([margins.left, width - margins.right - margins.left]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+      .attr("transform", "translate(0,5)rotate(-45)")
+      .style("text-anchor", "end")
+  var y = d3.scaleBand()
+    .range([height - margins.bottom, margins.top])
+    .domain(data.map(function(d) {return d.Name_Of_Region}))
+    .padding(.1);
+  svg.selectAll("myRect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", x(0))
+    .attr("y", function(d) { return y(d.Name_Of_Region); })
+    .attr("width", function(d) { return x(d.Average_Deaths); })
+    .attr("height", y.bandwidth() )
+    .attr("fill", "#69b3a2")
+  svg.append("text")
+    .attr("class", "y labels")
+    .attr("text-anchor", "end")
+    .attr("y", (function(d) {return x(d.Average_Deaths)}))
+    .attr("y" , (function (d) { return y(d.Name_Of_Region);}))
+    .text("Countries")
+})
 
   
 
