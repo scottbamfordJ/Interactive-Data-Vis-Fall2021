cdf80b9da4c92fbba0dbc6a3a083d0baf3e7c854
scottbamfordJ <75759730+scottbamfordJ@users.noreply.github.com>
2021-10-06 19:24:13 -0400

    2_2 Distributions

--- 2_2_distributions/main.js
+++ 2_2_distributions/main.js
@@ -1,43 +1,69 @@
 /* CONSTANTS AND GLOBALS */
-const width = 900,
-  height = 700,
-  margin = 20,
-  radius = 1;
-
+const width = 600,
+    height = 600,
+    margin = 50,
+    radius = 5;
 /* LOAD DATA */
-d3.csv("UniqueTerroristGroups.csv", d3.autoType)
+d3.csv("CountryData.csv", d3.autoType)
   .then(data => {
     console.log(data)
-    console.log(data, d => d[5])
 
     const svg = d3.select("#container")
       .append("svg")
       .attr("width", width)
       .attr("height", height)
-      .style("background-color", "pink")
     /* SCALES */
+    const yScale = d3.scaleLinear()
+      .domain(d3.extent(data, d => d.Number_Of_Attacks)).nice()
+      .range([height - margin, margin])
+
     const xScale = d3.scaleLinear()
-      .domain(d3.extent(data, d=> d.Number_Of_Attacks))
+      .domain(d3.extent(data, d => d.Number_Of_Deaths))
       .range([margin, width - margin])
       .nice()
-    console.log('xScale.range() :>> ', xScale.range())
-    const yScale = d3.scaleLinear()
-      .domain(d3.extent(data, d=> d.Number_Of_Deaths))
-      .range([height - margin, margin])
-    
-    // const colorScale = d3.scaleOrdinal()
-    //   .domain(data.map(d => d.Terrorist_Organization))
-    //   .range(["green", "blue", "purple", "white", "black", "red", ])
-    /* HTML ELEMENTS */
+    const rScale = d3.scaleLinear()
+      .domain(d3.extent(data, d => d.Average_Deaths))
+      .range([1,10])
+    console.log(yScale(4));
+
+    const colorScale = d3.scaleOrdinal()
+      .domain(data.map(d => d.Name_Of_Region))
+      .range(d3.schemeSet3);
+      /* HTML ELEMENTS */
+    svg.selectAll("mydots")
+      .data(data)
+      .enter()
+      .append("circle")
+        .attr("cx",200)
+        .attr("cy", function (d,i){ return 45 + i*25})
+        .attr("r", 6)
+        .style("fill", d => colorScale(d.Name_Of_Region))
     svg.selectAll(".dot")
       .data(data)
       .join("circle")
       .attr("class", "dot")
-      .attr("cs", d => xScale(d.Number_Of_Attacks))
-      .attr("cy", d => yScale(d.Number_of_Deaths))
-      .attr("r", radius)
-      //.style("fill", d => colorScale(d.Terrorist_Organization))
-    svg.append("g")
-      .attr("class", "x-axis")
+      .attr("cx", d => xScale(d.Number_Of_Deaths))
+      .attr("cy", d => yScale(d.Number_Of_Attacks))
+      .attr("r", d => rScale(d.Average_Deaths))
+      .style("fill", d => colorScale(d.Name_Of_Region))
 
+  
+    svg.selectAll("mylabels")
+      .data(data)
+      .enter()
+      .append("text")
+        .attr("x", 300)
+        .attr("y", function (d,i) {return 50 + i * 25})
+        .style("function", function(d) {return colorScale(d)})
+        .text(function(d) { return d.Name_Of_Region})
+        .attr("text-anchor", "middle")
+        .style("alignment-baseline", "middle")
+      svg.append("g")
+      .attr("class", "x-axis")
+      .style("transform", `translate(0px,${height - margin}px)`)
+      .call(d3.axisBottom(xScale))
+    svg.append("g")
+      .attr("class", "y-axis")
+      .style("transform", `translate(${margin}px,0px)`)
+      .call(d3.axisLeft(yScale))
   });
