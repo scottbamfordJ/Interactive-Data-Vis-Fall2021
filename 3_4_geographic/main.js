f73d37f53f9229f6b60b9b5573398192cfcd1026
scottbamfordJ <75759730+scottbamfordJ@users.noreply.github.com>
2021-11-02 17:15:19 -0400

    Update

--- 3_4_geographic/main.js
+++ 3_4_geographic/main.js
@@ -1,12 +1,20 @@
 /**
  * CONSTANTS AND GLOBALS
  * */
- const width = window.innerWidth * 0.9,
-    height = window.innerHeight * 0.7,
+ const width = 1000,
+    height = 800,
     margin = { top: 20, bottom: 50, left: 60, right: 40 };
 
 let svg, 
-    hoverBox;
+    hoverBox,
+    svg2;
+let xScale;
+let yScale;
+let xAxis;
+let xAxisGroup;
+let yAxis;
+let yAxisGroup;
+let colorScale
 /**
 * APPLICATION STATE
 * */
@@ -14,6 +22,8 @@ let svg,
 let state = {
     geojson: [],
     terrorist_state_data: [],
+    all_attacks: [],
+    selected_state: "Select A State"
     };
 
 /**
@@ -22,11 +32,13 @@ let state = {
 * */
 Promise.all([
  d3.json("../data/usState.json"),
- d3.csv("../Project/Data/State_Data.csv")
-]).then(([geojson, terrorist_state_data]) => {
+ d3.csv("../Project/Data/State_Data.csv"),
+ d3.csv("../Project/Data/UnitedStatesTerrorism.csv")
+]).then(([geojson, terrorist_state_data, full_data]) => {
  state.geojson = geojson
- state.terrorist_state_data = terrorist_state_data;
- //console.log("state: ", state);
+ state.terrorist_state_data = terrorist_state_data
+ state.full_data = full_data
+//  console.log("state: ", state);
  init();
 });
 
@@ -41,7 +53,10 @@ function init() {
 const attacksLookup = new Map(state.terrorist_state_data.map(d=> [
     d['States'], d['number_of_attacks']
 ]))
-console.log('attacksLookup :>>', attacksLookup.get("California"));
+
+const statelookup = new Map(state.full_data.map(d=> [d['province'] , d['iyear']]))
+
+console.log('statelookup :>>', statelookup);
 svg = d3.select("#container")
     .append("svg")
     .attr("width", width)
@@ -50,7 +65,11 @@ hoverBox = d3.select("#hover-content")
 
 const projection = d3.geoAlbersUsa()
     .fitSize([width, height], state.geojson)
+const projections_world_to_state = d3.geoAlbersUsa()
+    .translate([width, height])
+    .scale(500)
 const pathGen = d3.geoPath().projection(projection);
+
 
 const usSates = svg.selectAll("path.state")
     .data(state.geojson.features)
@@ -61,11 +80,26 @@ const usSates = svg.selectAll("path.state")
         // console.log(d)
         return colorScale(+attacksLookup.get(d.properties.NAME))
     })
-usSates.on("mousemove", (ev, d) => {
-    state.hover_state = d.properties.NAME
-    state.hover_attacks = attacksLookup.get(d.properties.NAME) 
+const storage = state.full_data
+console.log(storage)
+usSates.on("click", (ev, d) => {
+    state.click_state = d.properties.NAME
+    state.click_info = storage.filter(function(d){
+        return d.provstate == state.click_state
+    });
+    console.log(state.click_state)
+    console.log(state.click_info)
     draw();
 })
+
+
+// usSates.on("mousemove", (ev, d) => {
+//     console.log(d)
+//     state.hover_state = d.properties.NAME
+//     state.hover_attacks = attacksLookup.get(d.properties.NAME) 
+//     draw();
+// })
+
 svg.on("mousemove", (ev) => {
     const [mx, my] = d3.pointer(ev)
     state.x = ev.clientX;
@@ -74,6 +108,48 @@ svg.on("mousemove", (ev) => {
     state.longitude = projection.invert([mx, my])[1];
     draw()
 })
+// Drop Down Idea
+// const test2 = "California"
+// tests = [...new Set(state.full_data.map(d => d.provstate))]
+// console.log(tests)
+
+// const selectElement = d3.select("#dropdown")
+// let unique_outputs = [...new Set(state.full_data.map(d => d.provstate))]
+// xScale = d3.scaleLinear()
+//     .domain(d3.extent(state.data, d => d.ideologyScore2020))
+//     .range([20, width - 20])
+
+// yScale = d3.scaleLinear()
+//     .domain(d3.extent(state.data, d => d.envScore2020))
+//     .range([height - 20, 20])
+
+// xAxis = d3.axisBottom(xScale)
+// yAxis = d3.axisLeft(yScale)
+
+// console.log(unique_outputs)
+// selectElement
+//     .selectAll("options")
+//     .data(unique_outputs)
+//     .join("option")
+//     .attr("value", d => d)
+//     .text(d => d)
+// selectElement.on("change", event =>{
+//     state.selectedParty = event.target.value
+//     draw();
+// })
+// svg1 = d3.select("#bargraph")
+//     .append("svg")
+//     .attr("width", width)
+//     .attr("height", height)
+// xAxisGroup = svg1.append("g")
+//     .attr("class", 'xAxis')
+//     .attr("transform", `translate(${0}, ${height - 20})`) // move to the bottom
+//     .call(xAxis)
+
+//   yAxisGroup = svg.append("g")
+//     .attr("class", 'yAxis')
+//     .attr("transform", `translate(${20}, ${0})`) // align with left margin
+//     .call(yAxis)
  draw(); // calls the draw function
 }
 
@@ -89,5 +165,23 @@ function draw() {
             `<div>US State: ${state.hover_state}</div>
             <div>Number of Attacks: ${state.hover_attacks}</div>`
           )
+    
+}   
 
-}
+
+
+/** 
+ * Creating New BarChart Which shows the Groups involved in the specific attacks
+ */
+
+// let svg2, xScale, yScale, XAxis, xAxisGroup, yAxisGroup, yAxis, colorScale2
+// let states = {
+//     data: [],
+//     selected_state : []
+// }
+
+// d3.csv("../Project/Data/UnitedStatesTerrorism.csv", d3.autoType.then(raw_data =>{
+//     state.data = raw_data;
+//     console.log('state:>>', state);
+
+// })
